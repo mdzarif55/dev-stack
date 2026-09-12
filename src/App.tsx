3e035451f4eca1a30c769ddd39d0d2a0c020c415
mdzarif55 <muuhtasimzarif55.com>
2026-09-12 22:37:40 +0600

    type declar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Hero from './components/hero';
+import Technologies from './components/Technologies';
 
+const techFetch = async () => {
+  const res = fetch('/data.json');
+  const data = (await res).json();
+  return data;
+};
 
 function App() {
 
+  const techPromise = techFetch();
 
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Suspense fallback={<h2>Loading....</h2>}>
+        <Technologies techPromise= {techPromise} />
+      </Suspense>
       <Footer/>
     </>
   )
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react'
+
+const Technologies = ({techPromise}) => {
+    console.log(techPromise);
+    const tech= use(techPromise);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Technologies
